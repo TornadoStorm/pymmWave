fd354b55912d57e009a2a1b41fa28764f2326a74
Julian Dühnen (s2219735) <j.b.duhnen@student.utwente.nl>
2024-09-27 09:33:17 +0200

    Minor refactoring

--- src/pymmWave/parsing/__init__.py
+++
@@ -1,9 +0,0 @@
-from area_scanner.area_scanner_parser import AreaScannerParser
-from area_scanner.models import DynamicPoint, StaticPoint, TrackedObject
-
-__all__ = [
-    "AreaScannerParser",
-    "DynamicPoint",
-    "StaticPoint",
-    "TrackedObject",
-]
