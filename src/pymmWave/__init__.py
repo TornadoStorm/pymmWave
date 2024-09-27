2778fcdcae75a4efc70fb74ec14806a9249356f7
Julian Dühnen (s2219735) <j.b.duhnen@student.utwente.nl>
2024-09-27 09:08:35 +0200

    Refactoring - Moved setup.py details to pyproject.toml - Removed out_of_box_parser as it is broken code

---
+++ src/pymmWave/__init__.py
@@ -0,0 +1,6 @@
+import parsing
+from data_model import DataModel, DopplerPointCloud
+from IWR6843AOP import IWR6843AOP
+from sensor import Sensor
+
+__all__ = ["DataModel", "DopplerPointCloud", "IWR6843AOP", "Sensor", "parsing"]
---
+++ src/pymmWave/parsing/__init__.py
@@ -0,0 +1,9 @@
+from area_scanner.area_scanner_parser import AreaScannerParser
+from area_scanner.models import DynamicPoint, StaticPoint, TrackedObject
+
+__all__ = [
+    "AreaScannerParser",
+    "DynamicPoint",
+    "StaticPoint",
+    "TrackedObject",
+]
